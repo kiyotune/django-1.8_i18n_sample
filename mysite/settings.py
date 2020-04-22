c3f5c524ac2d4d9ce2a2e1917928739fcb8d9ca2
kiyotune@protein.osaka-u.ac.jp <kiyotune@protein.osaka-u.ac.jp>
2020-04-22 12:26:47 +0900

    'django-amdin makemessages -l ja' でエラーになるためまだ動かない。

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,6 +49,11 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+		'django.middleware.locale.LocaleMiddleware',
+)
+
+LOCALE_PATHS = (
+		os.path.join.(BASE_DIR, 'locale'),
 )
 
 ROOT_URLCONF = 'mysite.urls'
